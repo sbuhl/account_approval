f6f04b0423faa9738759fcc6e50843babd04fcd3
Sébastien Bühl <buhlsebastien@gmail.com>
2021-12-13 09:14:34 +0200

    Changement license

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,21 @@
+# -*- coding: utf-8 -*-
+
 {
     'name': "Approvals Statement",
     'version': '1.0',
     'depends': ['base', 'hr_contract', 'account_accountant'],
+    
     'author': "Sebastien Buhl",
+    'website': "http://www.buhl.be",
+    'license': 'LGPL-3',
+
     'application': True,
     'category': 'Category',
+    'summary': 'Statements Approvals',
     'description': """
-        Description text
+        Add an approval step on the bank statement
     """,
+    
     'data': [
         'data/activity.xml',
         'security/account_approval_security.xml',
